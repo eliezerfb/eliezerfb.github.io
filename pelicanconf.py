8d65c5a857b6951ee144280e8f2c7752ab5eb516
Eliézer Bourchardt <eliezerfb@gmail.com>
2018-03-31 13:35:27 -0300

    domain altered

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Eliézer F. Bourchardt'
 SITENAME = 'Eliézer Bourchardt Personal Blog'
-SITEURL = ''
+SITEURL = 'eliezerfb.eti.br'
 
 PATH = 'content'
 
